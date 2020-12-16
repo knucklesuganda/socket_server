fa3f18db874b62f41130762f04c8f306abe80e99
silver <silveron.silveron@mail.ru>
2020-12-16 21:51:55 +0300

    ПОЛУЧЕНИЕ ПОЛНОГО СООБЩЕНИЯ. СОКЕТ ЧАТ #1

--- Socket.py
+++ Socket.py
@@ -1,5 +1,6 @@
 import socket
 import asyncio
+import struct
 
 
 class Socket:
@@ -16,8 +17,28 @@ class Socket:
     async def send_data(self, data=None):
         raise NotImplementedError()
 
-    async def listen_socket(self, listened_socket=None):
-        raise NotImplementedError()
+    async def _recv_message(self, listened_socket: socket.socket, message_len: int):
+        message = bytearray()
+
+        while len(message) < message_len:
+            packet = await self.main_loop.sock_recv(listened_socket, message_len - len(message))
+            if packet is None:
+                return None
+
+            message.extend(packet)
+
+        return message
+
+    async def _decode_data(self, data: bytes):
+        return data.decode("utf-8")
+
+    async def listen_socket(self, listened_socket):
+        meta_data = await self._recv_message(listened_socket, 4)
+        meta_data = struct.unpack("I>", meta_data)[0]
+        print("need to get:", meta_data)
+
+        data = await self._recv_message(listened_socket, meta_data)
+        return self._decode_data(data)
 
     async def main(self):
         raise NotImplementedError()
